ad6734633cbb7a9dfb17bc96dcf9b01a9cd715b4
Correa1310 <146136159+Correa1310@users.noreply.github.com>
2023-10-18 13:32:33 -0700

    left off at 3.2.4

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float JumpForce = 10f;
+    public float GravityModifier = 1f;
+    public bool IsOnGround = true;
+    public bool gameOver = false;
+
+    private Rigidbody _playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();
+        //_playerRb.AddForce(Vector3.up * 1000);
+        Physics.gravity *= GravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        {
+            _playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            IsOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            IsOnGround = true;
+        }
+    }
+}
